5a7035910c9cef88bdba24ea488e9a86d3ab1c38
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-01-20 16:01:40 +0000

    Add tests for V-EZ context creation

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 
 #include "scene/scene.hpp"
+#include "renderer/vez/vez_backend.hpp"
+#include "platform/win32_platform.hpp"
 
 using namespace goma;
 
@@ -51,6 +53,55 @@ TEST(SceneTest, CanDeleteAndRecreateNodes) {
     node = s.CreateNode(s.GetRootNode());
     ASSERT_EQ(node->id, NodeIndex(1, 2));
     ASSERT_EQ(node->parent_id, s.GetRootNode()->id);
+}
+
+class VezBackendTest : public ::testing::Test {
+  protected:
+    VezBackend vez;
+
+    void SetUp() override {
+        auto init_context_result = vez.InitContext();
+        ASSERT_TRUE(init_context_result)
+            << init_context_result.error().message();
+    }
+};
+
+TEST_F(VezBackendTest, CanInitializeContext) {}
+
+TEST_F(VezBackendTest, CanInitializeWin32Surface) {
+    Win32Platform platform;
+    platform.InitWindow();
+
+    auto init_surface_result = vez.InitSurface(&platform);
+    ASSERT_TRUE(init_surface_result) << init_surface_result.error().message();
+}
+
+TEST_F(VezBackendTest, CanCreatePipeline) {
+    static const char* vertex_shader_glsl = R"(
+#version 450
+
+layout(location = 0) in vec3 inPosition;
+
+void main() {
+    gl_Position = vec4(gl_InstanceIndex * 0.1 * inPosition, 1.0);
+}
+)";
+
+    static const char* fragment_shader_glsl = R"(
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(location = 0) out vec4 outColor;
+
+void main() {
+    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+)";
+
+    auto create_pipeline_result =
+        vez.GetGraphicsPipeline(vertex_shader_glsl, fragment_shader_glsl);
+    ASSERT_TRUE(create_pipeline_result)
+        << create_pipeline_result.error().message();
 }
 
 }  // namespace

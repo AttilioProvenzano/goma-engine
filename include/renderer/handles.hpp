34c1d441a86961f479da0aacbf2021c1205e56e1
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-01-22 19:28:30 +0000

    Make VulkanImage POD and add const qualifiers

--- include/renderer/handles.hpp
+++ include/renderer/handles.hpp
@@ -10,20 +10,16 @@ namespace goma {
 struct VulkanImage {
     VkImage image = VK_NULL_HANDLE;
     VkImageView image_view = VK_NULL_HANDLE;
-
-    VulkanImage(VkImage image_ = VK_NULL_HANDLE,
-                VkImageView image_view_ = VK_NULL_HANDLE)
-        : image(image_), image_view(image_view_) {}
 };
 
 struct Image {
-    VulkanImage vez = {};
+    const VulkanImage vez = {};
 
     Image(VulkanImage vez_) : vez(vez_) {}
 };
 
 struct Framebuffer {
-    VezFramebuffer vez = {};
+    const VezFramebuffer vez = {};
 
     Framebuffer(VezFramebuffer vez_) : vez(vez_) {}
 };
@@ -93,7 +89,7 @@ struct FramebufferDesc {
 };
 
 struct Pipeline {
-    VezPipeline vez = VK_NULL_HANDLE;
+    const VezPipeline vez = VK_NULL_HANDLE;
 
     Pipeline(VezPipeline vez_) : vez(vez_) {}
 };

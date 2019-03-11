6c74368ad94903e830b9b56845aee3200f21104a
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-03-11 21:50:02 +0000

    Set camera default aspect ratio

--- include/scene/attachments/camera.hpp
+++ include/scene/attachments/camera.hpp
@@ -14,7 +14,7 @@ struct Camera {
     float h_fov = 60.0f;
     float near_plane = 0.1f;
     float far_plane = 1000.0f;
-    float aspect_ratio = 0.0f;
+    float aspect_ratio = 1.78f;
 
 	glm::vec3 position = glm::vec3(0.0f);
     glm::vec3 up = {0.0f, 1.0f, 0.0f};

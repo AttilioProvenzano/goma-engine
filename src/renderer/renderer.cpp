d870030058f2632972d9b4a85fa2bc31fe2b2941
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-03-11 21:51:22 +0000

    Fix default shader

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -315,13 +315,13 @@ void main() {
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
-// layout(set = 0, binding = 0) uniform sampler2D mainTexture;
+layout(set = 0, binding = 0) uniform sampler2D mainTexture;
 
 layout(location = 0) in vec2 inUVs;
 layout(location = 0) out vec4 outColor;
 
 void main() {
-    outColor = vec4(inUVs, 1.0, 1.0); // texture(mainTexture, inUVs);
+    outColor = texture(mainTexture, vec2(1.0, -1.0) * inUVs);
 }
 )";
 

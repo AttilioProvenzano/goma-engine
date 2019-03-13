befb46fc0f476de982101ea127ab465e24d9a94c
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-03-13 22:13:59 +0000

    Update the default vertex shader to support mesh buffers

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -369,10 +369,39 @@ result<void> Renderer::Render() {
         static const char* vert = R"(
 #version 450
 
+#ifdef HAS_POSITIONS
 layout(location = 0) in vec3 inPosition;
-layout(location = 1) in vec2 inUVs;
+#endif
 
-layout(location = 0) out vec2 outUVs;
+#ifdef HAS_NORMALS
+layout(location = 1) in vec3 inNormal;
+#endif
+
+#ifdef HAS_TANGENTS
+layout(location = 2) in vec3 inTangent;
+#endif
+
+#ifdef HAS_BITANGENTS
+layout(location = 3) in vec3 inBitangent;
+#endif
+
+#ifdef HAS_COLORS
+layout(location = 4) in vec4 inColor;
+#endif
+
+#ifdef HAS_UV0
+layout(location = 5) in vec2 inUV0;
+#endif
+
+#ifdef HAS_UV1
+layout(location = 6) in vec2 inUV1;
+#endif
+
+#ifdef HAS_UVW
+layout(location = 7) in vec2 inUVW;
+#endif
+
+layout(location = 0) out vec2 outUV0;
 
 layout(push_constant) uniform PC {
 	mat4 mvp;
@@ -380,7 +409,7 @@ layout(push_constant) uniform PC {
 
 void main() {
     gl_Position = pc.mvp * vec4(inPosition, 1.0);
-    outUVs = inUVs;
+    outUV0 = inUV0;
 }
 )";
 

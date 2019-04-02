aa52ace6144e5c1fd7e812556a552a94b5f7a6ed
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-04-02 23:48:03 +0100

    Move main includes to a separate file

---
+++ include/common/include.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <outcome.hpp>
+namespace outcome = OUTCOME_V2_NAMESPACE;
+using outcome::result;
+
+#include <array>
+#include <functional>
+#include <map>
+#include <memory>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <typeindex>
+#include <unordered_map>
+#include <vector>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
---
+++ include/common/vez.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VK_NO_PROTOTYPES
+#include "VEZ.h"
+#include <volk.h>

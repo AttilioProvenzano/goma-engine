c1d970d68dca6e4061c379c11abaead2a39f7b52
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-04-04 17:42:33 +0100

    Add a main loop and input system to Engine

--- include/engine.hpp
+++ include/engine.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "input/input_system.hpp"
 #include "renderer/renderer.hpp"
 #include "platform/platform.hpp"
 #include "scene/scene.hpp"
@@ -10,15 +11,24 @@ class Engine {
   public:
     Engine();
 
+    result<void> MainLoop(MainLoopFn inner_fn);
     result<void> LoadScene(const char* file_path);
 
     Platform* platform() { return platform_.get(); }
+    InputSystem* input_system() { return input_system_.get(); }
     Renderer* renderer() { return renderer_.get(); }
     Scene* scene() { return scene_.get(); }
 
+    uint32_t frame_count() { return frame_count_; }
+
   private:
-    std::unique_ptr<Renderer> renderer_;
+    uint32_t frame_count_ = 0;
+
     std::unique_ptr<Platform> platform_;
+
+    std::unique_ptr<InputSystem> input_system_;
+    std::unique_ptr<Renderer> renderer_;
+
     std::unique_ptr<Scene> scene_;
 };
 
--- src/engine.cpp
+++ src/engine.cpp
@@ -5,10 +5,32 @@
 
 namespace goma {
 
-Engine::Engine() : platform_(std::make_unique<Win32Platform>()) {
+Engine::Engine()
+    : platform_(std::make_unique<Win32Platform>()),
+      input_system_(std::make_unique<InputSystem>(*platform_.get())) {
     platform_->InitWindow();
     renderer_ = std::make_unique<Renderer>(this);
 }
+
+result<void> Engine::MainLoop(MainLoopFn inner_loop) {
+    if (platform_) {
+        platform_->MainLoop([&]() {
+            input_system_->AcquireFrameInput();
+            renderer_->Render();
+
+            bool res = false;
+            if (inner_loop) {
+                // Inner loop allows for conditional termination
+                res = inner_loop();
+            }
+
+            frame_count_++;
+            return res;
+        });
+    }
+
+    return outcome::success();
+};
 
 result<void> Engine::LoadScene(const char* file_path) {
     AssimpLoader loader;

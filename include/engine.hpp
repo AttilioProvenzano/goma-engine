6d976e36699781d0af1c6f94a555f8b267c19fe2
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-04-08 22:39:58 +0100

    Add scripting system and main camera management

--- include/engine.hpp
+++ include/engine.hpp
@@ -2,8 +2,10 @@
 
 #include "input/input_system.hpp"
 #include "renderer/renderer.hpp"
+#include "scripting/scripting_system.hpp"
 #include "platform/platform.hpp"
 #include "scene/scene.hpp"
+#include "scene/attachments/camera.hpp"
 
 namespace goma {
 
@@ -16,8 +18,10 @@ class Engine {
 
     Platform* platform() { return platform_.get(); }
     InputSystem* input_system() { return input_system_.get(); }
+    ScriptingSystem* scripting_system() { return scripting_system_.get(); }
     Renderer* renderer() { return renderer_.get(); }
     Scene* scene() { return scene_.get(); }
+    AttachmentIndex<Camera> main_camera() { return main_camera_; }
 
     uint32_t frame_count() { return frame_count_; }
 
@@ -27,9 +31,13 @@ class Engine {
     std::unique_ptr<Platform> platform_;
 
     std::unique_ptr<InputSystem> input_system_;
+    std::unique_ptr<ScriptingSystem> scripting_system_;
     std::unique_ptr<Renderer> renderer_;
 
     std::unique_ptr<Scene> scene_;
+    AttachmentIndex<Camera> main_camera_;
+
+    result<AttachmentIndex<Camera>> CreateDefaultCamera();
 };
 
 }  // namespace goma
--- src/engine.cpp
+++ src/engine.cpp
@@ -2,6 +2,7 @@
 
 #include "platform/win32_platform.hpp"
 #include "scene/loaders/assimp_loader.hpp"
+#include "scripting/scripts/fly_camera.hpp"
 
 namespace goma {
 
@@ -9,6 +10,7 @@ Engine::Engine()
     : platform_(std::make_unique<Win32Platform>()),
       input_system_(std::make_unique<InputSystem>(*platform_.get())) {
     platform_->InitWindow();
+    scripting_system_ = std::make_unique<ScriptingSystem>(*this);
     renderer_ = std::make_unique<Renderer>(this);
 }
 
@@ -16,6 +18,7 @@ result<void> Engine::MainLoop(MainLoopFn inner_loop) {
     if (platform_) {
         platform_->MainLoop([&]() {
             input_system_->AcquireFrameInput();
+            scripting_system_->Update(0.016f);  // TODO delta_time
             renderer_->Render();
 
             bool res = false;
@@ -37,7 +40,30 @@ result<void> Engine::LoadScene(const char* file_path) {
     OUTCOME_TRY(scene, loader.ReadSceneFromFile(file_path));
     scene_ = std::move(scene);
 
+    OUTCOME_TRY(main_camera, CreateDefaultCamera());
+    main_camera_ = main_camera;
+
+    FlyCamera fly_camera(main_camera_, 100.0f);
+    scripting_system_->RegisterScript(std::move(fly_camera));
+
     return outcome::success();
+}
+
+result<AttachmentIndex<Camera>> Engine::CreateDefaultCamera() {
+    Camera camera = {};
+    camera.aspect_ratio = float(platform_->GetWidth()) / platform_->GetHeight();
+
+    auto new_camera_res = scene_->CreateAttachment<Camera>(std::move(camera));
+    if (!new_camera_res) {
+        return Error::NoMainCamera;
+    }
+
+    auto new_camera_id = new_camera_res.value();
+    // Create a node for the new camera
+    auto camera_node = scene_->CreateNode(scene_->GetRootNode()).value();
+    scene_->Attach<Camera>(new_camera_id, camera_node);
+
+    return new_camera_id;
 }
 
 }  // namespace goma

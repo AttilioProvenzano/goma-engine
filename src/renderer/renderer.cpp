c6896bd89ef39881ba9b3f8c6fe8a37b551afecc
Attilio Provenzano <attilio.provenzano@gmail.com>
2019-04-15 21:11:37 +0100

    Remove shaders from code and load them from file

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -437,121 +437,12 @@ result<void> Renderer::Render() {
                 }
                 auto& material = *material_result.value();
 
-                static const char* vert = R"(
-#version 450
-
-#ifdef HAS_POSITIONS
-layout(location = 0) in vec3 inPosition;
-#endif
-
-#ifdef HAS_NORMALS
-layout(location = 1) in vec3 inNormal;
-#endif
-
-#ifdef HAS_TANGENTS
-layout(location = 2) in vec3 inTangent;
-#endif
-
-#ifdef HAS_BITANGENTS
-layout(location = 3) in vec3 inBitangent;
-#endif
-
-#ifdef HAS_COLORS
-layout(location = 4) in vec4 inColor;
-#endif
-
-#ifdef HAS_UV0
-layout(location = 5) in vec2 inUV0;
-#endif
-
-#ifdef HAS_UV1
-layout(location = 6) in vec2 inUV1;
-#endif
-
-#ifdef HAS_UVW
-layout(location = 7) in vec2 inUVW;
-#endif
-
-layout(location = 0) out vec2 outUV0;
-
-layout(push_constant) uniform PC {
-	mat4 mvp;
-} pc;
-
-void main() {
-    gl_Position = pc.mvp * vec4(inPosition, 1.0);
-    outUV0 = inUV0;
-}
-)";
-
-                static const char* frag = R"(
-#version 450
-#extension GL_ARB_separate_shader_objects : enable
-
-#ifdef HAS_DIFFUSE_MAP
-layout(set = 0, binding = 0) uniform sampler2D diffuseTex;
-#endif
-
-#ifdef HAS_SPECULAR_MAP
-layout(set = 0, binding = 1) uniform sampler2D specularTex;
-#endif
-
-#ifdef HAS_AMBIENT_MAP
-layout(set = 0, binding = 2) uniform sampler2D ambientTex;
-#endif
-
-#ifdef HAS_EMISSIVE_MAP
-layout(set = 0, binding = 3) uniform sampler2D emissiveTex;
-#endif
-
-#ifdef HAS_METALLIC_ROUGHNESS_MAP
-layout(set = 0, binding = 4) uniform sampler2D metallicRoughnessTex;
-#endif
-
-#ifdef HAS_HEIGHT_MAP
-layout(set = 0, binding = 5) uniform sampler2D heightTex;
-#endif
-
-#ifdef HAS_NORMAL_MAP
-layout(set = 0, binding = 6) uniform sampler2D normalTex;
-#endif
-
-#ifdef HAS_SHININESS_MAP
-layout(set = 0, binding = 7) uniform sampler2D shininessTex;
-#endif
-
-#ifdef HAS_OPACITY_MAP
-layout(set = 0, binding = 8) uniform sampler2D opacityTex;
-#endif
-
-#ifdef HAS_DISPLACEMENT_MAP
-layout(set = 0, binding = 9) uniform sampler2D displacementTex;
-#endif
-
-#ifdef HAS_LIGHT_MAP
-layout(set = 0, binding = 10) uniform sampler2D lightTex;
-#endif
-
-#ifdef HAS_REFLECTION_MAP
-layout(set = 0, binding = 11) uniform sampler2D reflectionTex;
-#endif
-
-layout(location = 0) in vec2 inUVs;
-layout(location = 0) out vec4 outColor;
-
-void main() {
-#ifdef false
-    outColor = texture(normalTex, vec2(1.0, -1.0) * inUVs);
-#else
-    outColor = texture(diffuseTex, vec2(1.0, -1.0) * inUVs);
-#endif
-}
-)";
-
                 auto pipeline_res = backend_->GetGraphicsPipeline(
-                    {vert, ShaderSourceType::Source,
+                    {"../../../assets/shaders/base.vert",
+                     ShaderSourceType::Filename,
                      GetVertexShaderPreamble(*mesh)},
-                    {frag, ShaderSourceType::Source,
+                    {"../../../assets/shaders/base.frag",
+                     ShaderSourceType::Filename,
                      GetFragmentShaderPreamble(material)});
                 if (!pipeline_res) {
                     spdlog::error("Couldn't get pipeline for material {}.",
@@ -583,39 +474,10 @@ void main() {
         }
 
         // Draw skybox
-        static const char* skybox_vert = R"(
-#version 450
-
-layout(push_constant) uniform PC {
-	mat4 mvp;
-} pc;
-
-layout(location = 0) in vec3 inPosition;
-layout(location = 0) out vec3 outUVW;
-
-void main() {
-    gl_Position = pc.mvp * vec4(inPosition, 0.0);
-	outUVW = inPosition;
-}
-)";
-
-        static const char* skybox_frag = R"(
-#version 450
-#extension GL_ARB_separate_shader_objects : enable
-
-layout(set = 0, binding = 0) uniform samplerCube diffuseTex;
-
-layout(location = 0) in vec3 inUVW;
-layout(location = 0) out vec4 outColor;
-
-void main() {
-    outColor = texture(diffuseTex, inUVW);
-}
-)";
-
         auto pipeline_res = backend_->GetGraphicsPipeline(
-            {skybox_vert, ShaderSourceType::Source},
-            {skybox_frag, ShaderSourceType::Source});
+            {"../../../assets/shaders/skybox.vert", ShaderSourceType::Filename},
+            {"../../../assets/shaders/skybox.frag",
+             ShaderSourceType::Filename});
         if (!pipeline_res) {
             spdlog::error("Couldn't get pipeline for skybox.");
             // TODO return

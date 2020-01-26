0d200c79b3d08cc3dd9a7c082e390d1a70d53337
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-01-26 20:11:08 +0000

    Move VMA and VK_CHECK to a common header

--- modules/common/include/common/vulkan.hpp
+++ modules/common/include/common/vulkan.hpp
@@ -2,4 +2,36 @@
 
 #define VK_NO_PROTOTYPES
 #include <vulkan/vulkan.h>
+#include <vma/vk_mem_alloc.h>
 #include <volk.h>
+
+#include "common/error_codes.hpp"
+
+#ifndef VK_CHECK
+#define VK_CHECK(fn)                                                         \
+    {                                                                        \
+        VkResult _r = fn;                                                    \
+        if (_r != VK_SUCCESS) {                                              \
+            spdlog::error(                                                   \
+                "{}, line {}: In function {}, a Vulkan error occurred when " \
+                "running {}.",                                               \
+                __FILE__, __LINE__, __func__, #fn);                          \
+        };                                                                   \
+        switch (_r) {                                                        \
+            case VK_SUCCESS:                                                 \
+                break;                                                       \
+            case VK_ERROR_INITIALIZATION_FAILED:                             \
+                return Error::VulkanInitializationFailed;                    \
+            case VK_ERROR_OUT_OF_HOST_MEMORY:                                \
+                return Error::OutOfCPUMemory;                                \
+            case VK_ERROR_OUT_OF_DEVICE_MEMORY:                              \
+                return Error::OutOfGPUMemory;                                \
+            case VK_ERROR_LAYER_NOT_PRESENT:                                 \
+                return Error::VulkanLayerNotPresent;                         \
+            case VK_ERROR_EXTENSION_NOT_PRESENT:                             \
+                return Error::VulkanExtensionNotPresent;                     \
+            default:                                                         \
+                return Error::GenericVulkanError;                            \
+        }                                                                    \
+    }
+#endif

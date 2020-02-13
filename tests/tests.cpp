aedeaa6fd6b876eb648366bf6a08eb2d9915d7bd
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-02-13 23:01:33 +0000

    Add shaders for HelloTriangle

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -29,6 +29,35 @@ using namespace goma;
 
 namespace {
 
+static const char* triangle_vtx = R"(
+#version 450
+
+layout(location = 0) out vec3 outColor;
+
+vec3 triangle[3] = vec3[](
+    vec3(1.0, -1.0, 0.0),
+    vec3(-1.0, 1.0, 0.0),
+    vec3(1.0, 1.0, 0.0)
+);
+
+void main() {
+    gl_Position = vec4(triangle[gl_VertexIndex], 1.0);
+    outColor = vec3(1.0, 0.0, 0.0);
+}
+)";
+
+static const char* triangle_frag = R"(
+#version 450
+
+layout(location = 0) in vec3 inColor;
+
+layout(location = 0) out vec4 outColor;
+
+void main() {
+    outColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+)";
+
 static const char* vtx = R"(
 #version 450
 

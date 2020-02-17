cf4f484e5a5961e8249f4fcb292ab07176d87a58
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-02-17 21:56:53 +0000

    Add new test shaders

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -42,7 +42,9 @@ vec3 triangle[3] = vec3[](
 
 void main() {
     gl_Position = vec4(triangle[gl_VertexIndex], 1.0);
-    outColor = vec3(1.0, 0.0, 0.0);
+
+    outColor = vec3(0.0, 0.0, 0.0);
+    outColor[gl_VertexIndex] = 1.0;
 }
 )";
 
@@ -54,7 +56,7 @@ layout(location = 0) in vec3 inColor;
 layout(location = 0) out vec4 outColor;
 
 void main() {
-    outColor = vec4(1.0, 0.0, 0.0, 1.0);
+    outColor = vec4(inColor, 1.0);
 }
 )";
 
@@ -62,13 +64,28 @@ static const char* vtx = R"(
 #version 450
 
 layout(location = 0) in vec3 inPosition;
-layout(location = 1) in vec2 inUVs;
+layout(location = 1) in vec3 inColor;
+layout(location = 2) in vec2 inUVs;
 
-layout(location = 0) out vec2 outUVs;
+layout(location = 0) out vec3 outColor;
+layout(location = 1) out vec2 outUVs;
 
 void main() {
     gl_Position = vec4(inPosition, 1.0);
     outUVs = inUVs;
+}
+)";
+
+static const char* frag = R"(
+#version 450
+
+layout(location = 0) in vec3 inColor;
+layout(location = 1) in vec2 inUVs;
+
+layout(location = 0) out vec4 outColor;
+
+void main() {
+    outColor = vec4(inColor, 1.0);
 }
 )";
 

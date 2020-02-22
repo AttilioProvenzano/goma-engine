d32bea5e1955b4208a3de8b0d8c4acb42e153003
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-02-22 16:44:28 +0000

    Add tests for descriptor sets and Spinning Cube

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -75,8 +75,9 @@ layout(binding = 0, std140) uniform UBO {
 } ubo;
 
 void main() {
-    gl_Position = vec4(inPosition, 1.0);
+    gl_Position = ubo.mvp * vec4(inPosition, 1.0);
     outUVs = inUVs;
+    outColor = inColor;
 }
 )";
 
@@ -93,16 +94,80 @@ void main() {
 }
 )";
 
+using Color = glm::vec3;
+
 struct Vertex {
     glm::vec3 pos;
+    Color color;
     glm::vec2 uv;
 };
 
-std::vector<Vertex> buf_data = {{{1.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
-                                {{0.0f, 1.0f, 1.0f}, {1.0f, 1.0f}}};
+const glm::vec3 kFrontTopLeft = {-1.0f, 1.0f, -1.0f};
+const glm::vec3 kFrontBottomLeft = {-1.0f, -1.0f, -1.0f};
+const glm::vec3 kFrontTopRight = {1.0f, 1.0f, -1.0f};
+const glm::vec3 kFrontBottomRight = {1.0f, -1.0f, -1.0f};
+const glm::vec3 kBackTopLeft = {-1.0f, 1.0f, 1.0f};
+const glm::vec3 kBackBottomLeft = {-1.0f, -1.0f, 1.0f};
+const glm::vec3 kBackTopRight = {1.0f, 1.0f, 1.0f};
+const glm::vec3 kBackBottomRight = {1.0f, -1.0f, 1.0f};
+
+const Color kRed = {1.0f, 0.0f, 0.0f};
+const Color kBlue = {0.0f, 0.0f, 1.0f};
+const Color kGreen = {0.0f, 1.0f, 0.0f};
+const Color kYellow = {1.0f, 1.0f, 0.0f};
+const Color kWhite = {1.0f, 1.0f, 1.0f};
+const Color kPurple = {1.0f, 0.0f, 1.0f};
+
+std::vector<Vertex> cube_vtx_data = {
+    // Front
+    {kFrontTopLeft, kRed, {0.0f, 0.0f}},
+    {kFrontTopRight, kRed, {0.0f, 0.0f}},
+    {kFrontBottomLeft, kRed, {0.0f, 0.0f}},
+    {kFrontBottomRight, kRed, {0.0f, 0.0f}},
+
+    // Back
+    {kBackTopLeft, kGreen, {0.0f, 0.0f}},
+    {kBackTopRight, kGreen, {0.0f, 0.0f}},
+    {kBackBottomLeft, kGreen, {0.0f, 0.0f}},
+    {kBackBottomRight, kGreen, {0.0f, 0.0f}},
+
+    // Left
+    {kFrontTopLeft, kBlue, {0.0f, 0.0f}},
+    {kFrontBottomLeft, kBlue, {0.0f, 0.0f}},
+    {kBackTopLeft, kBlue, {0.0f, 0.0f}},
+    {kBackBottomLeft, kBlue, {0.0f, 0.0f}},
+
+    // Right
+    {kFrontTopRight, kYellow, {0.0f, 0.0f}},
+    {kFrontBottomRight, kYellow, {0.0f, 0.0f}},
+    {kBackTopRight, kYellow, {0.0f, 0.0f}},
+    {kBackBottomRight, kYellow, {0.0f, 0.0f}},
+
+    // Top
+    {kFrontTopLeft, kWhite, {0.0f, 0.0f}},
+    {kFrontTopRight, kWhite, {0.0f, 0.0f}},
+    {kBackTopLeft, kWhite, {0.0f, 0.0f}},
+    {kBackTopRight, kWhite, {0.0f, 0.0f}},
+
+    // Bottom
+    {kFrontBottomLeft, kPurple, {0.0f, 0.0f}},
+    {kFrontBottomRight, kPurple, {0.0f, 0.0f}},
+    {kBackBottomLeft, kPurple, {0.0f, 0.0f}},
+    {kBackBottomRight, kPurple, {0.0f, 0.0f}},
+};
+
+std::vector<uint32_t> cube_index_data = {
+    0,  2,  3,  3,  1,  0,   // Front
+    4,  5,  6,  5,  7,  6,   // Back
+    8,  10, 11, 11, 9,  8,   // Left
+    12, 13, 14, 15, 14, 13,  // Right
+    16, 17, 18, 18, 17, 19,  // Top
+    20, 22, 21, 22, 23, 21,  // Bottom
+};
 
 constexpr int kWindowWidth = 1024;
 constexpr int kWindowHeight = 768;
+constexpr int kTimeoutSeconds = 1;
 
 TEST(GlslangTest, CanCompileShader) {
     using namespace glslang;
@@ -171,9 +236,9 @@ TEST_F(RendererTest, CanCreateDevice) {
 result<Buffer*> CreateBufferTest(Device& device, VmaMemoryUsage storage) {
     BufferDesc desc = {};
     desc.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-    desc.num_elements = static_cast<uint32_t>(buf_data.size());
-    desc.stride = sizeof(buf_data[0]);
-    desc.size = buf_data.size() * sizeof(buf_data[0]);
+    desc.num_elements = static_cast<uint32_t>(cube_vtx_data.size());
+    desc.stride = sizeof(cube_vtx_data[0]);
+    desc.size = cube_vtx_data.size() * sizeof(cube_vtx_data[0]);
     desc.storage = storage;
 
     OUTCOME_TRY(buffer, device.CreateBuffer(desc));
@@ -181,7 +246,8 @@ result<Buffer*> CreateBufferTest(Device& device, VmaMemoryUsage storage) {
     UploadContext ctx(device);
     OUTCOME_TRY(ctx.Begin());
     OUTCOME_TRY(ctx.UploadBuffer(
-        *buffer, {buf_data.size() * sizeof(buf_data[0]), buf_data.data()}));
+        *buffer, {cube_vtx_data.size() * sizeof(cube_vtx_data[0]),
+                  cube_vtx_data.data()}));
     ctx.End();
 
     OUTCOME_TRY(receipt, device.Submit(ctx));
@@ -248,6 +314,44 @@ TEST_F(RendererTest, CanCreateShaderAndPipeline) {
     ASSERT_NE(pipeline->GetHandle(), VkPipeline{VK_NULL_HANDLE});
 }
 
+TEST_F(RendererTest, CanBindDescriptorSet) {
+    // Create a buffer
+    BufferDesc desc = {};
+    desc.usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+    desc.num_elements = static_cast<uint32_t>(cube_vtx_data.size());
+    desc.stride = sizeof(cube_vtx_data[0]);
+    desc.size = cube_vtx_data.size() * sizeof(cube_vtx_data[0]);
+    desc.storage = VMA_MEMORY_USAGE_CPU_TO_GPU;
+
+    GOMA_TEST_TRY(buffer, device->CreateBuffer(desc));
+
+    UploadContext ctx(*device);
+    GOMA_TEST_TRYV(ctx.Begin());
+    GOMA_TEST_TRYV(ctx.UploadBuffer(
+        *buffer, {cube_vtx_data.size() * sizeof(cube_vtx_data[0]),
+                  cube_vtx_data.data()}));
+    ctx.End();
+
+    GOMA_TEST_TRY(receipt, device->Submit(ctx));
+    GOMA_TEST_TRYV(device->WaitOnWork(std::move(receipt)));
+
+    // Create a pipeline
+    ShaderDesc shader_desc = {};
+    shader_desc.name = "vtx";
+    shader_desc.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    shader_desc.source = vtx;
+
+    GOMA_TEST_TRY(shader, device->CreateShader(std::move(shader_desc)));
+    GOMA_TEST_TRY(pipeline,
+                  device->CreatePipeline({{shader}}, FramebufferDesc{}));
+
+    GraphicsContext context(*device);
+    GOMA_TEST_TRYV(context.Begin());
+    context.BindGraphicsPipeline(*pipeline);
+    context.BindDescriptorSet({{0, *buffer}});
+    context.End();
+}
+
 TEST_F(RendererTest, CanCreateGraphicsContext) {
     GraphicsContext context(*device);
     GOMA_TEST_TRYV(context.Begin());
@@ -255,15 +359,10 @@ TEST_F(RendererTest, CanCreateGraphicsContext) {
 }
 
 void HelloTriangle(Device* device, bool offscreen) {
-    ShaderDesc vtx_desc = {};
-    vtx_desc.name = "vtx";
-    vtx_desc.stage = VK_SHADER_STAGE_VERTEX_BIT;
-    vtx_desc.source = triangle_vtx;
-
-    ShaderDesc frag_desc = {};
-    frag_desc.name = "frag";
-    frag_desc.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-    frag_desc.source = triangle_frag;
+    ShaderDesc vtx_desc = {"triangle_vtx", VK_SHADER_STAGE_VERTEX_BIT,
+                           triangle_vtx};
+    ShaderDesc frag_desc = {"triangle_frag", VK_SHADER_STAGE_FRAGMENT_BIT,
+                            triangle_frag};
 
     GOMA_TEST_TRY(vtx, device->CreateShader(std::move(vtx_desc)));
     GOMA_TEST_TRY(frag, device->CreateShader(std::move(frag_desc)));
@@ -334,12 +433,147 @@ TEST_F(RendererGraphicalTest, CanCreateWindow) {
 
 TEST_F(RendererGraphicalTest, HelloTriangle) {
     HelloTriangle(device.get(), false);
-    Sleep(1000);
+    Sleep(kTimeoutSeconds * 1000);
 }
 
-// TEST_F(RendererTest, SpinningCube) {}
-// TEST_F(RendererTest, OffscreenRendering) {}
-// TEST_F(RendererTest, Screenshot) {}
+TEST_F(RendererGraphicalTest, SpinningCube) {
+    BufferDesc desc = {};
+    desc.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+    desc.num_elements = static_cast<uint32_t>(cube_vtx_data.size());
+    desc.stride = sizeof(cube_vtx_data[0]);
+    desc.size = cube_vtx_data.size() * sizeof(cube_vtx_data[0]);
+    desc.storage = VMA_MEMORY_USAGE_GPU_ONLY;
+    GOMA_TEST_TRY(vtx_buf, device->CreateBuffer(desc));
+
+    desc.usage = VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
+    desc.num_elements = static_cast<uint32_t>(cube_index_data.size());
+    desc.stride = sizeof(cube_index_data[0]);
+    desc.size = cube_index_data.size() * sizeof(cube_index_data[0]);
+    desc.storage = VMA_MEMORY_USAGE_GPU_ONLY;
+    GOMA_TEST_TRY(index_buf, device->CreateBuffer(desc));
+
+    UploadContext upload_ctx(*device);
+    GOMA_TEST_TRYV(upload_ctx.Begin());
+    GOMA_TEST_TRYV(upload_ctx.UploadBuffer(
+        *vtx_buf, {cube_vtx_data.size() * sizeof(cube_vtx_data[0]),
+                   cube_vtx_data.data()}));
+    GOMA_TEST_TRYV(upload_ctx.UploadBuffer(
+        *index_buf, {cube_index_data.size() * sizeof(cube_index_data[0]),
+                     cube_index_data.data()}));
+    upload_ctx.End();
+
+    GOMA_TEST_TRY(upload_receipt, device->Submit(upload_ctx));
+    GOMA_TEST_TRYV(device->WaitOnWork(std::move(upload_receipt)));
+
+    ShaderDesc vtx_desc = {"vtx", VK_SHADER_STAGE_VERTEX_BIT, vtx};
+    ShaderDesc frag_desc = {"frag", VK_SHADER_STAGE_FRAGMENT_BIT, frag};
+
+    GOMA_TEST_TRY(vtx, device->CreateShader(std::move(vtx_desc)));
+    GOMA_TEST_TRY(frag, device->CreateShader(std::move(frag_desc)));
+
+    auto depth_desc = ImageDesc::DepthAttachmentDesc;
+    depth_desc.size = {platform->GetWidth(), platform->GetHeight(), 1};
+    GOMA_TEST_TRY(depth_image, device->CreateImage(depth_desc));
+
+    int frame_index;
+    std::vector<ReceiptPtr> receipts(3);
+
+    // TODO: move to a single buffer, bind with offset
+    std::vector<Buffer*> mvp_bufs;
+
+    std::unordered_map<Image*, FramebufferDesc> fb_desc;
+
+    GraphicsContext context(*device);
+    auto start_time = std::chrono::steady_clock::now();
+
+    const int kMaxFrames = 5000;
+    for (int frame = 0; frame < kMaxFrames; frame++) {
+        frame_index = frame % 3;
+
+        if (mvp_bufs.size() <= frame_index) {
+            desc.usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+            desc.num_elements = 1;
+            desc.stride = sizeof(glm::mat4);
+            desc.size = sizeof(glm::mat4);
+            desc.storage = VMA_MEMORY_USAGE_CPU_TO_GPU;
+            GOMA_TEST_TRY(mvp_buf, device->CreateBuffer(desc));
+
+            mvp_bufs.push_back(mvp_buf);
+        }
+
+        if (receipts[frame_index]) {
+            GOMA_TEST_TRYV(
+                device->WaitOnWork(std::move(receipts[frame_index])));
+        }
+        context.NextFrame();
+
+        GOMA_TEST_TRY(swapchain_image, device->AcquireSwapchainImage());
+
+        auto fb_res = fb_desc.find(swapchain_image);
+        if (fb_res == fb_desc.end()) {
+            FramebufferDesc fb = {};
+            fb.color_attachments.push_back({swapchain_image});
+            fb.depth_attachment = {depth_image,
+                                   VK_ATTACHMENT_LOAD_OP_CLEAR,
+                                   VK_ATTACHMENT_STORE_OP_DONT_CARE,
+                                   {1.0f, 0}};
+
+            fb_desc[swapchain_image] = fb;
+            fb_res = fb_desc.find(swapchain_image);
+        }
+
+        PipelineDesc pipe_desc = {{vtx, frag}};
+        pipe_desc.depth_test = true;
+        pipe_desc.cull_mode = VK_CULL_MODE_BACK_BIT;
+        GOMA_TEST_TRY(pipeline, device->CreatePipeline(std::move(pipe_desc),
+                                                       fb_res->second));
+
+        auto eye = glm::vec3(0.0f, -5.0f, 0.0f);
+        auto center = glm::vec3(0.0f);
+        auto up = glm::vec3{0.0f, 0.0f, 1.0f};
+        auto rot = glm::quat(static_cast<float>(frame) *
+                             glm::vec3{glm::radians(0.1f), glm::radians(0.2f),
+                                       glm::radians(0.3f)});
+
+        auto mvp = glm::perspective(glm::radians(60.0f),
+                                    static_cast<float>(platform->GetWidth()) /
+                                        platform->GetHeight(),
+                                    0.1f, 100.0f) *
+                   glm::lookAt(eye, center, up) * glm::mat4_cast(rot);
+
+        GOMA_TEST_TRY(mvp_data, device->MapBuffer(*mvp_bufs[frame_index]));
+        memcpy(mvp_data, &mvp, sizeof(mvp));
+        device->UnmapBuffer(*mvp_bufs[frame_index]);
+
+        GOMA_TEST_TRYV(context.Begin());
+        GOMA_TEST_TRYV(context.BindFramebuffer(fb_res->second));
+
+        context.BindGraphicsPipeline(*pipeline);
+        context.BindDescriptorSet({{0, *mvp_bufs[frame_index]}});
+
+        context.BindVertexBuffer(*vtx_buf);
+        context.BindIndexBuffer(*index_buf);
+        context.DrawIndexed(static_cast<uint32_t>(cube_index_data.size()));
+
+        context.End();
+
+        GOMA_TEST_TRY(receipt, device->Submit(context));
+        receipts[frame_index] = std::move(receipt);
+
+        GOMA_TEST_TRYV(device->Present());
+    }
+
+    auto elapsed_time = std::chrono::steady_clock::now() - start_time;
+
+    spdlog::info("Average FPS: {}", (1e9 * kMaxFrames) / elapsed_time.count());
+
+    for (auto& receipt : receipts) {
+        if (receipt) {
+            GOMA_TEST_TRYV(device->WaitOnWork(std::move(receipt)));
+        }
+    }
+}
+
 // TEST_F(RendererTest, GUI) {}
 
 }  // namespace

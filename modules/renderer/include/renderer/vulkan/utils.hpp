faa85e74ec250f11a433f8e5fc9d81fa51b18c0b
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-02-22 21:53:55 +0000

    Implement detection of format info

--- modules/renderer/include/renderer/vulkan/utils.hpp
+++ modules/renderer/include/renderer/vulkan/utils.hpp
@@ -12,5 +12,12 @@ namespace utils {
 result<VkRenderPass> CreateRenderPass(VkDevice device,
                                       const FramebufferDesc& desc);
 
+struct FormatInfo {
+    uint32_t size;
+    uint32_t channel_count;
+};
+
+FormatInfo GetFormatInfo(VkFormat format);
+
 }  // namespace utils
 }  // namespace goma

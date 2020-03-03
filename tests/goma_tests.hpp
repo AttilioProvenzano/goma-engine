e5a5870e36a01e022273718daf6d47bd1857a64a
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-03-03 00:52:16 +0000

    Simplify error handling to try and avoid C1001 bug

--- tests/goma_tests.hpp
+++ tests/goma_tests.hpp
@@ -7,19 +7,24 @@
 #endif
 
 #ifndef GOMA_TEST_TRYV
-#define GOMA_TEST_TRYV(fn)                                         \
-    {                                                              \
-        auto res = fn;                                             \
-        if (res.has_error()) UNSCOPED_INFO(res.error().message()); \
-        REQUIRE(!res.has_error());                                 \
+#define GOMA_TEST_TRYV(fn)                \
+    {                                     \
+        auto res = fn;                    \
+        if (res.has_error()) {            \
+            auto err = res.error();       \
+            UNSCOPED_INFO(err.message()); \
+        }                                 \
+        REQUIRE(!res.has_error());        \
     }
 #endif
 
 #ifndef GOMA_TEST_TRY
-#define GOMA_TEST_TRY(handle, fn)                      \
-    auto handle##_res = fn;                            \
-    if (handle##_res.has_error())                      \
-        UNSCOPED_INFO(handle##_res.error().message()); \
-    REQUIRE(!handle##_res.has_error());                \
+#define GOMA_TEST_TRY(handle, fn)                 \
+    auto handle##_res = fn;                       \
+    if (handle##_res.has_error()) {               \
+        auto handle##_err = handle##_res.error(); \
+        UNSCOPED_INFO(handle##_err.message());    \
+    }                                             \
+    REQUIRE(!handle##_res.has_error());           \
     auto& handle = handle##_res.value();
 #endif

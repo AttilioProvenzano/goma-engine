6d886e5c54e863253a32f2990381dd5999368d65
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-03-03 19:58:29 +0000

    Add function info to GOMA_TEST_TRY

--- tests/goma_tests.hpp
+++ tests/goma_tests.hpp
@@ -7,27 +7,27 @@
 #endif
 
 #ifndef GOMA_TEST_TRYV
-#define GOMA_TEST_TRYV(fn)                \
-    {                                     \
-        auto res = fn;                    \
-        if (res.has_error()) {            \
-            auto err = res.error();       \
-            UNSCOPED_INFO(err.message()); \
-        }                                 \
-        REQUIRE(!res.has_error());        \
+#define GOMA_TEST_TRYV(fn)                                        \
+    {                                                             \
+        auto res = fn;                                            \
+        if (res.has_error()) {                                    \
+            auto err = res.error();                               \
+            UNSCOPED_INFO("In " << #fn << ": " << err.message()); \
+        }                                                         \
+        REQUIRE(!res.has_error());                                \
     }
 #endif
 
 #ifndef GOMA_TEST_TRY
-#define GOMA_TEST_TRY(handle, fn)                         \
-    auto handle##_res = fn;                               \
-    {                                                     \
-        bool handle##_has_err = handle##_res.has_error(); \
-        if (handle##_has_err) {                           \
-            auto handle##_err = handle##_res.error();     \
-            UNSCOPED_INFO(handle##_err.message());        \
-        }                                                 \
-        REQUIRE(!handle##_has_err);                       \
-    }                                                     \
+#define GOMA_TEST_TRY(handle, fn)                                          \
+    auto handle##_res = fn;                                                \
+    {                                                                      \
+        bool handle##_has_err = handle##_res.has_error();                  \
+        if (handle##_has_err) {                                            \
+            auto handle##_err = handle##_res.error();                      \
+            UNSCOPED_INFO("In " << #fn << ": " << handle##_err.message()); \
+        }                                                                  \
+        REQUIRE(!handle##_has_err);                                        \
+    }                                                                      \
     auto& handle = handle##_res.value();
 #endif

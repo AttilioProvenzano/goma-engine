95458412af3ed715cf827299b6c03eba32254cd3
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-03-05 18:53:26 +0000

    Set spdlog level to debug

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,2 +1,10 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+#include <spdlog/spdlog.h>
+
+int main(int argc, char* argv[]) {
+    spdlog::set_level(spdlog::level::debug);
+    int result = Catch::Session().run(argc, argv);
+    return result;
+}

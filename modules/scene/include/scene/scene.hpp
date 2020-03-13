ebe195e7af6c4f4f148dfdddc23d933df93145aa
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-03-13 22:49:50 +0000

    Adjust Scene to the new gen_vector and Node structure

--- modules/scene/include/scene/scene.hpp
+++ modules/scene/include/scene/scene.hpp
@@ -1,128 +1,40 @@
 #pragma once
 
-#include "scene/node.hpp"
-#include "scene/attachment.hpp"
-#include "scene/attachments/texture.hpp"
-
 #include "common/include.hpp"
+#include "common/gen_vector.hpp"
 
-template <typename T>
-using TypeMap = std::unordered_map<std::type_index, T>;
+#include "scene/node.hpp"
+#include "scene/attachments/camera.hpp"
+#include "scene/attachments/light.hpp"
+#include "scene/attachments/material.hpp"
+#include "scene/attachments/mesh.hpp"
+#include "scene/attachments/texture.hpp"
 
 namespace goma {
 
 class Scene {
   public:
-    Scene();
-
-    result<NodeIndex> CreateNode(const NodeIndex parent,
-                                 const Transform& transform = Transform());
-    NodeIndex GetRootNode() { return nodes_[0].id; }
-    void DeleteNode(NodeIndex id);
-
-    result<NodeIndex> GetParent(NodeIndex id);
-    result<std::set<NodeIndex>> GetChildren(NodeIndex id);
-    result<Transform> GetTransform(NodeIndex id);
-    result<glm::mat4> GetTransformMatrix(NodeIndex id);
-    bool SetTransform(NodeIndex id, const Transform& transform);
-
     template <typename T>
-    result<AttachmentIndex<T>> CreateAttachment(const NodeIndex& node_id,
-                                                T&& data = T()) {
-        return GetAttachmentManager<T>()->Create({node_id},
-                                                 std::forward<T>(data));
-    }
+    using vec_type = gen_vector<T>;
 
-    template <typename T>
-    result<AttachmentIndex<T>> CreateAttachment(T&& data = T()) {
-        return GetAttachmentManager<T>()->Create(std::forward<T>(data));
-    }
+    Node& root_node() { return root_node_; }
 
-    template <typename T>
-    bool RegisterAttachment(AttachmentIndex<T> attachment,
-                            const std::string& name, bool overwrite = true) {
-        return GetAttachmentManager<T>()->Register(attachment, name, overwrite);
-    }
+    vec_type<Camera>& cameras() { return cameras_; }
+    vec_type<Light>& lights() { return lights_; }
+    vec_type<Material>& materials() { return materials_; }
+    vec_type<Mesh>& meshes() { return meshes_; }
+    vec_type<Texture>& textures() { return textures_; }
 
-    template <typename T>
-    void ForEach(std::function<void(const AttachmentIndex<T>,
-                                    const std::set<NodeIndex>&, T&)>
-                     fun) {
-        return GetAttachmentManager<T>()->ForEach(std::move(fun));
-    }
-
-    template <typename T>
-    void ForEach(std::function<void(T&)> fun) {
-        return GetAttachmentManager<T>()->ForEach(std::move(fun));
-    }
-
-    template <typename T>
-    const std::vector<Attachment<T>>& GetAttachments() {
-        return GetAttachmentManager<T>()->GetAll();
-    }
-
-    template <typename T>
-    result<std::reference_wrapper<T>> GetAttachment(AttachmentIndex<T> id) {
-        return GetAttachmentManager<T>()->Get(id);
-    }
-
-    template <typename T>
-    result<std::pair<AttachmentIndex<T>, std::reference_wrapper<T>>>
-    FindAttachment(const std::string& name) {
-        return GetAttachmentManager<T>()->Find(name);
-    }
-
-    template <typename T>
-    size_t GetAttachmentCount() {
-        return GetAttachmentManager<T>()->count();
-    }
-
-    template <typename T>
-    bool Attach(AttachmentIndex<T> id, NodeIndex node) {
-        return GetAttachmentManager<T>()->Attach(id, node);
-    }
-
-    template <typename T>
-    bool Detach(AttachmentIndex<T> id, NodeIndex node) {
-        return GetAttachmentManager<T>()->Detach(id, node);
-    }
-
-    template <typename T>
-    bool DetachAll(AttachmentIndex<T> id) {
-        return GetAttachmentManager<T>()->DetachAll(id);
-    }
-
-    template <typename T>
-    result<std::reference_wrapper<std::set<NodeIndex>>> GetAttachedNodes(
-        AttachmentIndex<T> id) {
-        return GetAttachmentManager<T>()->GetNodes(id);
-    }
+    Node* find(const std::string& name) { return root_node_.find(name); }
 
   private:
-    using AttachmentManagerMap =
-        TypeMap<std::unique_ptr<AttachmentManagerBase>>;
+    Node root_node_ = {"Root"};
 
-    std::vector<Node> nodes_{};
-    std::queue<size_t> recycled_nodes_{};
-
-    AttachmentManagerMap attachment_managers_{};
-
-    template <typename T>
-    AttachmentManager<T>* GetAttachmentManager() {
-        auto type_id = std::type_index(typeid(T));
-        auto result = attachment_managers_.find(type_id);
-        if (result != attachment_managers_.end()) {
-            return static_cast<AttachmentManager<T>*>(result->second.get());
-        }
-
-        attachment_managers_[type_id] =
-            std::make_unique<AttachmentManager<T>>();
-        return static_cast<AttachmentManager<T>*>(
-            attachment_managers_[type_id].get());
-    }
-
-    bool ValidateNode(NodeIndex id);
-    result<void> ComputeTransformMatrix(NodeIndex id);
+    vec_type<Camera> cameras_;
+    vec_type<Light> lights_;
+    vec_type<Material> materials_;
+    vec_type<Mesh> meshes_;
+    vec_type<Texture> textures_;
 };
 
 }  // namespace goma

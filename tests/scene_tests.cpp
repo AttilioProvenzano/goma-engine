0951128cec5651cd15299f582fdb4d3d04875ba3
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-03-13 22:51:55 +0000

    Update the scene tests

--- tests/scene_tests.cpp
+++ tests/scene_tests.cpp
@@ -18,57 +18,34 @@ SCENARIO("nodes can be created and destroyed safely", "[scene][node]") {
     GIVEN("an empty scene") {
         Scene s;
 
-        auto root_node = s.GetRootNode();
-        REQUIRE(root_node == NodeIndex{0, 1});
-        REQUIRE(s.GetChildren(root_node).value() == std::set<NodeIndex>{});
+        auto& root_node = s.root_node();
+        REQUIRE(root_node.children().empty());
 
         WHEN("a node is added") {
-            GOMA_TEST_TRY(node, s.CreateNode(root_node));
+            auto& node = root_node.add_child({"Node"});
 
-            THEN("it has index (1, 1), root as parent and no children") {
-                CHECK(node == NodeIndex{1, 1});
-                CHECK(s.GetParent(node).value() == root_node);
-                CHECK(s.GetChildren(node).value() == std::set<NodeIndex>{});
-            }
+            THEN("parent/child relationships are set correctly") {
+                CHECK(root_node.children().size() == 1);
+                CHECK(*root_node.children()[0] == node);
 
-            AND_WHEN("that node is deleted") {
-                s.DeleteNode(node);
-
-                THEN(
-                    "the root has no children and the node's parent is "
-                    "invalid") {
-                    CHECK(s.GetChildren(root_node).value() ==
-                          std::set<NodeIndex>{});
-                    CHECK(s.GetParent(node).has_error());
-                }
-
-                AND_WHEN("a new node is created in its place") {
-                    GOMA_TEST_TRY(new_node, s.CreateNode(root_node));
-
-                    THEN("the new node takes its index, with generation 2") {
-                        CHECK(new_node == NodeIndex(1, 2));
-                        CHECK(s.GetParent(new_node).value() == root_node);
-                    }
-                }
+                CHECK(node.parent() == &root_node);
+                CHECK(node.children().empty());
+                CHECK(node.name() == "Node");
             }
 
             AND_WHEN("a child node is added to it") {
-                GOMA_TEST_TRY(child_node, s.CreateNode(node));
+                auto& child = node.add_child({"Child"});
 
-                THEN(
-                    "it has index (2, 1) and parent/child relationships are "
-                    "set correctly") {
-                    CHECK(child_node == NodeIndex{2, 1});
-                    CHECK(s.GetParent(child_node).value() == NodeIndex{1, 1});
-                    CHECK(s.GetChildren(node).value() ==
-                          std::set<NodeIndex>{child_node});
+                THEN("parent/child relationships are set correctly") {
+                    CHECK(child.parent() == &node);
+                    CHECK(*node.children()[0] == child);
                 }
 
                 AND_WHEN("the original node is deleted") {
-                    s.DeleteNode(node);
+                    root_node.remove_child(node);
 
-                    THEN("the root node becomes the child node's parent") {
-                        CHECK(s.GetParent(child_node).value() == root_node);
+                    THEN("the root has no children anymore") {
+                        CHECK(root_node.children().empty());
                     }
                 }
             }
@@ -80,44 +57,43 @@ SCENARIO("attachments are handled correctly") {
     GIVEN("a scene with two nodes") {
         Scene s;
 
-        auto root_node = s.GetRootNode();
-        GOMA_TEST_TRY(node, s.CreateNode(s.GetRootNode()));
-        GOMA_TEST_TRY(other_node, s.CreateNode(s.GetRootNode()));
+        auto& root_node = s.root_node();
+        auto& node = root_node.add_child();
+        auto& other_node = root_node.add_child();
 
-        REQUIRE(s.GetChildren(root_node).value() ==
-                std::set<NodeIndex>{node, other_node});
+        REQUIRE(root_node.children().size() == 2);
 
         WHEN("a texture is created") {
-            GOMA_TEST_TRY(texture, s.CreateAttachment<Texture>({node}, {}));
-            auto& attached_nodes =
-                s.GetAttachedNodes<Texture>(texture).value().get();
+            auto tex = Texture{};
+            tex.attach_to(node);
+            s.textures().push_back(std::move(tex));
 
             THEN("the set of attached nodes is correctly populated") {
-                CHECK(attached_nodes == std::set<NodeIndex>({node}));
+                CHECK(tex.attached_nodes() == std::vector<Node*>{&node});
             }
 
             AND_WHEN("the texture is attached to a second node") {
-                REQUIRE(s.Attach<Texture>(texture, other_node));
+                tex.attach_to(other_node);
 
                 THEN("the set of attached nodes has both nodes") {
-                    CHECK(attached_nodes ==
-                          std::set<NodeIndex>({node, other_node}));
+                    CHECK(tex.attached_nodes() ==
+                          std::vector<Node*>{&node, &other_node});
                 }
 
                 AND_WHEN("it is detached from the first node") {
-                    REQUIRE(s.Detach<Texture>(texture, node));
+                    tex.detach_from(node);
 
                     THEN("the set of attached nodes has only the second one") {
-                        REQUIRE(attached_nodes ==
-                                std::set<NodeIndex>({other_node}));
+                        CHECK(tex.attached_nodes() ==
+                              std::vector<Node*>{&other_node});
                     }
                 }
 
                 AND_WHEN("all nodes are detached") {
-                    REQUIRE(s.DetachAll<Texture>(texture));
+                    tex.detach_all();
 
                     THEN("the set of attached nodes is empty") {
-                        REQUIRE(attached_nodes == std::set<NodeIndex>({}));
+                        CHECK(tex.attached_nodes().empty());
                     }
                 }
             }
@@ -135,27 +111,19 @@ SCENARIO("a model can be loaded via Assimp", "[scene][assimp]") {
                                                 "models/Duck/glTF/Duck.gltf"));
 
             THEN("node hierarchy and attachment counts are correct") {
-                CHECK(scene->GetAttachmentCount<Texture>() == 1);
-                CHECK(scene->GetAttachmentCount<Material>() == 1);
-                CHECK(scene->GetAttachmentCount<Camera>() == 1);
-                CHECK(scene->GetAttachmentCount<Light>() == 0);
-                CHECK(scene->GetAttachmentCount<Mesh>() == 1);
+                CHECK(scene->textures().size() == 2);
+                CHECK(scene->materials().size() == 1);
+                CHECK(scene->cameras().size() == 1);
+                CHECK(scene->lights().size() == 0);
+                CHECK(scene->meshes().size() == 1);
 
-                GOMA_TEST_TRY(attached_nodes,
-                              scene->GetAttachedNodes<Mesh>({0}));
-                CHECK(attached_nodes.get() == std::set<NodeIndex>({2}));
+                CHECK(scene->root_node().children().size() == 2);
 
-                GOMA_TEST_TRY(children,
-                              scene->GetChildren(scene->GetRootNode()));
-                CHECK(children.size() == 1);
+                REQUIRE(scene->meshes().is_valid({0, 0}));
+                auto& mesh = scene->meshes().at({0, 0});
 
-                auto assimp_root_node = children.begin();
-                GOMA_TEST_TRY(assimp_children,
-                              scene->GetChildren(*assimp_root_node));
-                CHECK(assimp_children.size() == 2);
-
-                GOMA_TEST_TRY(mesh_ref, scene->GetAttachment<Mesh>(0));
-                auto& mesh = mesh_ref.get();
+                REQUIRE(mesh.attached_nodes().size() == 1);
+                CHECK(mesh.attached_nodes()[0]->name() == "nodes_2");
 
                 auto& layout = mesh.vertices.layout;
                 auto stride = utils::GetStride(layout);

1a017912f7589aa5a4c31a7486783f72df2fe68c
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-03-13 22:49:20 +0000

    Rework Node to use a tree structure

--- modules/scene/include/scene/node.hpp
+++ modules/scene/include/scene/node.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "scene/gen_index.hpp"
-
 #include "common/include.hpp"
 
 namespace goma {
@@ -27,25 +25,120 @@ struct Transform {
     friend std::ostream& operator<<(std::ostream& o, const goma::Transform& t);
 };
 
-struct Node {
-    NodeIndex id;
-    NodeIndex parent;  // root node has parent {0, 0}
-    Transform transform{};
+class Node {
+  public:
+    using NodePtr = std::unique_ptr<Node>;
 
-    std::set<NodeIndex> children{};
-    std::unique_ptr<glm::mat4> cached_model{};
+    Node(const std::string& name = "", const Transform& t = {})
+        : parent_(nullptr), name_(name), transform_(t) {}
 
-    Node(NodeIndex id_, NodeIndex parent_,
-         const Transform& transform_ = Transform())
-        : id(id_), parent(parent_), transform(transform_) {}
+    const std::string& name() const { return name_; }
 
-    bool operator==(const Node& other) const { return (this->id == other.id); }
+    const Node* parent() const { return parent_; }
+    Node* parent() { return parent_; }
 
-    bool operator!=(const Node& other) const { return (this->id != other.id); }
+    const std::vector<NodePtr>& children() const { return children_; }
+    void set_children(std::vector<NodePtr>&& c) {
+        children_.swap(std::move(c));
+    }
 
-    bool valid() const { return id.valid(); }
+    Node& add_child(const std::string& name = "", const Transform& t = {}) {
+        auto child = std::make_unique<Node>(name, t);
+        child->parent_ = this;
 
-    friend std::ostream& operator<<(std::ostream& o, const Node& n);
+        children_.push_back(std::move(child));
+        return *children_.back().get();
+    }
+
+    void remove_child(Node& child) {
+        children_.erase(std::remove_if(children_.begin(), children_.end(),
+                                       [&child](const auto& c) {
+                                           return c.get() == &child;
+                                       }),
+                        children_.end());
+    }
+
+    void clear_children() { children_.clear(); }
+
+    const Transform& transform() const { return transform_; }
+    void set_transform(const Transform& t) {
+        transform_ = t;
+        invalidate_cached_transform_();
+    }
+
+    const glm::mat4& get_transform_matrix() {
+        if (!cached_transform_.valid) {
+            compute_transform_matrix_();
+        }
+        return cached_transform_.matrix;
+    }
+
+    Node* find(const std::string& name) {
+        if (name_ == name) {
+            return this;
+        }
+
+        for (const auto& c : children_) {
+            auto found = c->find(name);
+            if (found) {
+                return found;
+            }
+        }
+
+        return nullptr;
+    }
+
+    bool operator==(const Node& rhs) const {
+        auto& lhs = *this;
+        return lhs.name_ == rhs.name_ && lhs.parent_ == rhs.parent_ &&
+               lhs.transform_ == rhs.transform_;
+    }
+
+    bool operator!=(const Node& rhs) const { return !(*this == rhs); }
+
+  private:
+    Node* parent_;
+    std::string name_;
+    std::vector<NodePtr> children_;
+
+    Transform transform_;
+
+    struct {
+        glm::mat4 matrix;
+        bool valid = false;
+    } cached_transform_;
+
+    void invalidate_cached_transform_() {
+        cached_transform_.valid = false;
+
+        for (auto& child : children_) {
+            child->invalidate_cached_transform_();
+        }
+    }
+
+    void compute_transform_matrix_() {
+        std::vector<Node*> nodes_to_compute = {this};
+        auto n = parent_;
+        while (n != nullptr && !n->cached_transform_.valid) {
+            nodes_to_compute.push_back(n);
+            n = n->parent_;
+        }
+
+        auto current_matrix = n ? n->cached_transform_.matrix : glm::mat4{1.0f};
+
+        std::for_each(nodes_to_compute.rbegin(), nodes_to_compute.rend(),
+                      [&current_matrix](auto& n) {
+                          const auto& t = n->transform_;
+                          auto local_transform = glm::translate(t.position) *
+                                                 glm::mat4_cast(t.rotation) *
+                                                 glm::scale(t.scale);
+
+                          current_matrix = current_matrix * local_transform;
+
+                          n->cached_transform_.matrix = current_matrix;
+                          n->cached_transform_.valid = true;
+                      });
+    }
 };
 
 }  // namespace goma

cdf621d736dd966f3a61376b3a1a52f64edd6d71
Attilio Provenzano <attilio.provenzano@gmail.com>
2020-03-14 13:17:41 +0000

    Cache spv shaders

--- modules/rhi/vulkan/device.cpp
+++ modules/rhi/vulkan/device.cpp
@@ -2,7 +2,8 @@
 
 #include "glslang/Public/ShaderLang.h"
 #include "SPIRV/GlslangToSpv.h"
-#include <spirv_glsl.hpp>
+#include "spirv_glsl.hpp"
+#include "StandAlone/ResourceLimits.h"
 
 #include "common/error_codes.hpp"
 #include "platform/platform.hpp"
@@ -887,167 +888,89 @@ result<Sampler*> Device::CreateSampler(const SamplerDesc& sampler_desc) {
 }
 
 result<Shader*> Device::CreateShader(ShaderDesc shader_desc) {
-    static const std::unordered_map<VkShaderStageFlagBits, EShLanguage>
-        stage_map = {{VK_SHADER_STAGE_VERTEX_BIT, EShLangVertex},
-                     {VK_SHADER_STAGE_FRAGMENT_BIT, EShLangFragment}};
+    // Try to retrieve the SPIR-V from cache
+    struct ShaderSourceHasher {
+        size_t operator()(const ShaderDesc& desc) const {
+            size_t seed = 0;
+            hash_combine(seed, desc.stage);
+            hash_combine(seed, djb2_hash(desc.source.c_str()));
+            hash_combine(seed, djb2_hash(desc.preamble.c_str()));
+            return seed;
+        }
+    };
 
-    glslang::InitializeProcess();
-    glslang::TShader shader{stage_map.at(shader_desc.stage)};
+    std::stringstream filename;
+    filename << GOMA_CACHE_DIR "shaders/" << ShaderSourceHasher{}(shader_desc)
+             << ".spv";
 
-    auto source = shader_desc.source.c_str();
-    shader.setStrings(&source, 1);
-
-    if (!shader_desc.preamble.empty()) {
-        shader.setPreamble(shader_desc.preamble.c_str());
-    }
-
-    shader.setEnvInput(glslang::EShSourceGlsl, stage_map.at(shader_desc.stage),
-                       glslang::EShClientVulkan, 450);
-    shader.setEnvClient(glslang::EShClientVulkan, glslang::EShTargetVulkan_1_1);
-    shader.setEnvTarget(glslang::EShTargetSpv, glslang::EShTargetSpv_1_0);
-
-    static const TBuiltInResource kDefaultTBuiltInResource = {
-        /* .MaxLights = */ 32,
-        /* .MaxClipPlanes = */ 6,
-        /* .MaxTextureUnits = */ 32,
-        /* .MaxTextureCoords = */ 32,
-        /* .MaxVertexAttribs = */ 64,
-        /* .MaxVertexUniformComponents = */ 4096,
-        /* .MaxVaryingFloats = */ 64,
-        /* .MaxVertexTextureImageUnits = */ 32,
-        /* .MaxCombinedTextureImageUnits = */ 80,
-        /* .MaxTextureImageUnits = */ 32,
-        /* .MaxFragmentUniformComponents = */ 4096,
-        /* .MaxDrawBuffers = */ 32,
-        /* .MaxVertexUniformVectors = */ 128,
-        /* .MaxVaryingVectors = */ 8,
-        /* .MaxFragmentUniformVectors = */ 16,
-        /* .MaxVertexOutputVectors = */ 16,
-        /* .MaxFragmentInputVectors = */ 15,
-        /* .MinProgramTexelOffset = */ -8,
-        /* .MaxProgramTexelOffset = */ 7,
-        /* .MaxClipDistances = */ 8,
-        /* .MaxComputeWorkGroupCountX = */ 65535,
-        /* .MaxComputeWorkGroupCountY = */ 65535,
-        /* .MaxComputeWorkGroupCountZ = */ 65535,
-        /* .MaxComputeWorkGroupSizeX = */ 1024,
-        /* .MaxComputeWorkGroupSizeY = */ 1024,
-        /* .MaxComputeWorkGroupSizeZ = */ 64,
-        /* .MaxComputeUniformComponents = */ 1024,
-        /* .MaxComputeTextureImageUnits = */ 16,
-        /* .MaxComputeImageUniforms = */ 8,
-        /* .MaxComputeAtomicCounters = */ 8,
-        /* .MaxComputeAtomicCounterBuffers = */ 1,
-        /* .MaxVaryingComponents = */ 60,
-        /* .MaxVertexOutputComponents = */ 64,
-        /* .MaxGeometryInputComponents = */ 64,
-        /* .MaxGeometryOutputComponents = */ 128,
-        /* .MaxFragmentInputComponents = */ 128,
-        /* .MaxImageUnits = */ 8,
-        /* .MaxCombinedImageUnitsAndFragmentOutputs = */ 8,
-        /* .MaxCombinedShaderOutputResources = */ 8,
-        /* .MaxImageSamples = */ 0,
-        /* .MaxVertexImageUniforms = */ 0,
-        /* .MaxTessControlImageUniforms = */ 0,
-        /* .MaxTessEvaluationImageUniforms = */ 0,
-        /* .MaxGeometryImageUniforms = */ 0,
-        /* .MaxFragmentImageUniforms = */ 8,
-        /* .MaxCombinedImageUniforms = */ 8,
-        /* .MaxGeometryTextureImageUnits = */ 16,
-        /* .MaxGeometryOutputVertices = */ 256,
-        /* .MaxGeometryTotalOutputComponents = */ 1024,
-        /* .MaxGeometryUniformComponents = */ 1024,
-        /* .MaxGeometryVaryingComponents = */ 64,
-        /* .MaxTessControlInputComponents = */ 128,
-        /* .MaxTessControlOutputComponents = */ 128,
-        /* .MaxTessControlTextureImageUnits = */ 16,
-        /* .MaxTessControlUniformComponents = */ 1024,
-        /* .MaxTessControlTotalOutputComponents = */ 4096,
-        /* .MaxTessEvaluationInputComponents = */ 128,
-        /* .MaxTessEvaluationOutputComponents = */ 128,
-        /* .MaxTessEvaluationTextureImageUnits = */ 16,
-        /* .MaxTessEvaluationUniformComponents = */ 1024,
-        /* .MaxTessPatchComponents = */ 120,
-        /* .MaxPatchVertices = */ 32,
-        /* .MaxTessGenLevel = */ 64,
-        /* .MaxViewports = */ 16,
-        /* .MaxVertexAtomicCounters = */ 0,
-        /* .MaxTessControlAtomicCounters = */ 0,
-        /* .MaxTessEvaluationAtomicCounters = */ 0,
-        /* .MaxGeometryAtomicCounters = */ 0,
-        /* .MaxFragmentAtomicCounters = */ 8,
-        /* .MaxCombinedAtomicCounters = */ 8,
-        /* .MaxAtomicCounterBindings = */ 1,
-        /* .MaxVertexAtomicCounterBuffers = */ 0,
-        /* .MaxTessControlAtomicCounterBuffers = */ 0,
-        /* .MaxTessEvaluationAtomicCounterBuffers = */ 0,
-        /* .MaxGeometryAtomicCounterBuffers = */ 0,
-        /* .MaxFragmentAtomicCounterBuffers = */ 1,
-        /* .MaxCombinedAtomicCounterBuffers = */ 1,
-        /* .MaxAtomicCounterBufferSize = */ 16384,
-        /* .MaxTransformFeedbackBuffers = */ 4,
-        /* .MaxTransformFeedbackInterleavedComponents = */ 64,
-        /* .MaxCullDistances = */ 8,
-        /* .MaxCombinedClipAndCullDistances = */ 8,
-        /* .MaxSamples = */ 4,
-        /* .maxMeshOutputVerticesNV = */ 256,
-        /* .maxMeshOutputPrimitivesNV = */ 512,
-        /* .maxMeshWorkGroupSizeX_NV = */ 32,
-        /* .maxMeshWorkGroupSizeY_NV = */ 1,
-        /* .maxMeshWorkGroupSizeZ_NV = */ 1,
-        /* .maxTaskWorkGroupSizeX_NV = */ 32,
-        /* .maxTaskWorkGroupSizeY_NV = */ 1,
-        /* .maxTaskWorkGroupSizeZ_NV = */ 1,
-        /* .maxMeshViewCountNV = */ 4,
-
-        /* .limits = */
-        {
-            /* .nonInductiveForLoops = */ 1,
-            /* .whileLoops = */ 1,
-            /* .doWhileLoops = */ 1,
-            /* .generalUniformIndexing = */ 1,
-            /* .generalAttributeMatrixVectorIndexing = */ 1,
-            /* .generalVaryingIndexing = */ 1,
-            /* .generalSamplerIndexing = */ 1,
-            /* .generalVariableIndexing = */ 1,
-            /* .generalConstantMatrixVectorIndexing = */ 1,
-        }};
-
-    TBuiltInResource resource_limits = kDefaultTBuiltInResource;
-    bool compilation_res = shader.parse(
-        &resource_limits, 100, false,
-        static_cast<EShMessages>(EShMsgSpvRules | EShMsgVulkanRules));
-
-    if (!compilation_res) {
-        auto info_log = shader.getInfoLog();
-        spdlog::info("Shader \"{}\" - compilation failed:\n{}",
-                     shader_desc.name, info_log);
-
-        return Error::ShaderCompilationFailed;
-    }
-
-    glslang::TProgram program;
-    program.addShader(&shader);
-    bool link_res = program.link(EShMsgDefault);
-
-    if (!link_res) {
-        auto info_log = program.getInfoLog();
-        spdlog::info("Shader \"{}\" - linking failed:\n{}", shader_desc.name,
-                     info_log);
-    }
-
+    auto cached_spirv = Platform::ReadFile(filename.str(), true);
     std::vector<uint32_t> spirv;
-    spv::SpvBuildLogger logger;
-    glslang::SpvOptions spvOptions = {};
-    GlslangToSpv(*program.getIntermediate(stage_map.at(shader_desc.stage)),
-                 spirv, &logger, &spvOptions);
 
-    glslang::FinalizeProcess();
+    if (cached_spirv) {
+        spirv.resize(cached_spirv.value().size() / sizeof(spirv[0]));
+        memcpy(spirv.data(), cached_spirv.value().c_str(),
+               spirv.size() * sizeof(spirv[0]));
+    } else {
+        static const std::unordered_map<VkShaderStageFlagBits, EShLanguage>
+            stage_map = {{VK_SHADER_STAGE_VERTEX_BIT, EShLangVertex},
+                         {VK_SHADER_STAGE_FRAGMENT_BIT, EShLangFragment}};
+
+        glslang::InitializeProcess();
+        glslang::TShader shader{stage_map.at(shader_desc.stage)};
+
+        auto source = shader_desc.source.c_str();
+        shader.setStrings(&source, 1);
+
+        if (!shader_desc.preamble.empty()) {
+            shader.setPreamble(shader_desc.preamble.c_str());
+        }
+
+        shader.setEnvInput(glslang::EShSourceGlsl,
+                           stage_map.at(shader_desc.stage),
+                           glslang::EShClientVulkan, 450);
+        shader.setEnvClient(glslang::EShClientVulkan,
+                            glslang::EShTargetVulkan_1_1);
+        shader.setEnvTarget(glslang::EShTargetSpv, glslang::EShTargetSpv_1_0);
+
+        bool compilation_res = shader.parse(
+            &glslang::DefaultTBuiltInResource, 100, false,
+            static_cast<EShMessages>(EShMsgSpvRules | EShMsgVulkanRules));
+
+        if (!compilation_res) {
+            auto info_log = shader.getInfoLog();
+            spdlog::info("Shader \"{}\" - compilation failed:\n{}",
+                         shader_desc.name, info_log);
+
+            return Error::ShaderCompilationFailed;
+        }
+
+        glslang::TProgram program;
+        program.addShader(&shader);
+        bool link_res = program.link(EShMsgDefault);
+
+        if (!link_res) {
+            auto info_log = program.getInfoLog();
+            spdlog::info("Shader \"{}\" - linking failed:\n{}",
+                         shader_desc.name, info_log);
+        }
+
+        spv::SpvBuildLogger logger;
+        glslang::SpvOptions spvOptions = {};
+        GlslangToSpv(*program.getIntermediate(stage_map.at(shader_desc.stage)),
+                     spirv, &logger, &spvOptions);
+
+        glslang::FinalizeProcess();
+    }
 
     VkShaderModuleCreateInfo shader_info = {
         VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO};
     shader_info.codeSize = sizeof(spirv[0]) * spirv.size();
     shader_info.pCode = spirv.data();
+
+    // Cache the SPIR-V output
+    auto _ =
+        Platform::WriteFile(filename.str(), spirv.size() * sizeof(spirv[0]),
+                            reinterpret_cast<const char*>(spirv.data()), true);
 
     VkShaderModule shader_module = VK_NULL_HANDLE;
     VK_CHECK(vkCreateShaderModule(api_handles_.device, &shader_info, nullptr,
